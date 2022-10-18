12902a8b2ae6b554bde56c837e142a1856f0f16e
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-10-18 18:23:38 -0300

    feat: add validation yup

--- src/server/controller/cidades/Create.ts
+++ src/server/controller/cidades/Create.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from "express"
+import * as yup from 'yup';
 
 interface ICidades {
   nome: string
 }
 
-export const create = (req: Request<{}, {}, ICidades>, res: Response) => {
-  console.log(req.body);
-  return res.send('Create')
+const bodyValidation: yup.SchemaOf<ICidades> = yup.object().shape({
+  nome: yup.string().required().min(3)
+})
+
+export const create = async (req: Request<{}, {}, ICidades>, res: Response) => {
+  let validateDate: ICidades | undefined = undefined
+
+  try {
+    validateDate = await bodyValidation.validate(req.body)
+  } catch (error) {
+    const yupError = error as yup.ValidationError
+    return res.json({
+      error: {
+        default: yupError.message
+      }
+    })
+  }
+  console.log(validateDate);
+  return res.send(req.body)
 }
